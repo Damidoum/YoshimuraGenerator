cd5ac5189bfb38aea9c54e5816831763bc7a8a88
Damien Rouchouse <damien.rouchouse@etu.minesparis.psl.eu>
2024-05-31 11:31:46 -0400

    init project

---
+++ src/draw_yoshimora.py
@@ -0,0 +1,13 @@
+from dxfwrite import DXFEngine as dxf
+
+class Branch:
+    def __init__(self, length: float, angle: float, )
+
+class BuildingBlockYoshimora:
+    def __init__(
+        self, angle: float, branch_length: float, pannel_gap=1.2, beam_gap=2.33
+    ) -> None:
+        self.angle = angle
+        self.branch_length = branch_length
+        self.pannel_gap = pannel_gap
+        self.beam_gap = beam_gap
