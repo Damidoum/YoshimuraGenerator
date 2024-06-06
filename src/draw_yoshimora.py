d75016264267ad60b3bbfe24452ab68290a2d515
Damien Rouchouse <damien.rouchouse@etu.minesparis.psl.eu>
2024-06-06 14:01:39 -0400

    building blocks for shim

--- src/draw_yoshimora.py
+++ src/draw_yoshimora.py
@@ -157,6 +157,210 @@ class BranchTape(Branch):
         self.drawing.save()
 
 
+class CenterShim:
+    def __init__(
+        self,
+        center: tuple[float],
+        radius: float,
+        length: float,
+        angle: float,
+        ratio: float,
+        margin: float,
+        count_beam: int,
+        pannel_gap: 1.2,
+        beam_gap: 2.2,
+        activated_branch=[True for _ in range(6)],
+        beam_length=6.33,
+        beam_width=4.33,
+        drawing=dxf.drawing("yoshimora_shim.dxf"),
+    ):
+        self.center = center
+        self.radius = radius
+        self.length = length
+        self.angle = angle
+        self.ratio = ratio
+        self.margin = margin
+        self.count_beam = count_beam
+        self.pannel_gap = pannel_gap
+        self.beam_gap = beam_gap
+        self.activated_branch = activated_branch
+        self.beam_length = beam_length
+        self.beam_width = beam_width
+        self.drawing = drawing
+
+    def compute_branch_position(self) -> list[tuple[float]]:
+        branch_positions = []
+        angles = [0, self.angle, 180 - self.angle, 180, 180 + self.angle, -self.angle]
+        for i, angle in enumerate(angles):
+            point = end_point_of_line(self.center, self.radius, angle)
+            for j in range(i):
+                if j == 0:
+                    point = end_point_of_line(
+                        point,
+                        (self.beam_width * 1 / self.ratio - self.beam_width) / 2,
+                        angles[j] + 90,
+                    )
+                else:
+                    point = end_point_of_line(
+                        point,
+                        self.beam_width * 1 / self.ratio - self.beam_width,
+                        angles[j] + 90,
+                    )
+            # branch_positions.append(end_point_of_line(self.center, self.radius, angle))
+            branch_positions.append(point)
+        return branch_positions
+
+    def draw_shim(self):
+        width = self.beam_width * 1 / self.ratio
+        length_extremity_lines = (
+            self.length
+            - self.beam_length * self.count_beam
+            - self.beam_gap * (self.count_beam - 1)
+            - self.margin
+        ) / 2
+
+        branch_position = self.compute_branch_position()
+        angles = [0, self.angle, 180 - self.angle, 180, 180 + self.angle, -self.angle]
+        for i, branch_state in enumerate(self.activated_branch):
+            if branch_state:
+                if i == 0:
+                    start_point = end_point_of_line(
+                        branch_position[i],
+                        (width - self.beam_width + self.pannel_gap) / 2,
+                        angles[i] - 90,
+                    )
+                else:
+                    start_point = end_point_of_line(
+                        branch_position[i],
+                        self.pannel_gap / 2,
+                        angles[i] - 90,
+                    )
+                    self.drawing.add(dxf.line(start_point, end_shim))
+
+                shim_point1 = end_point_of_line(
+                    start_point, length_extremity_lines, angles[i]
+                )
+                shim_point2 = end_point_of_line(
+                    shim_point1,
+                    (self.beam_width - self.pannel_gap) / 2,
+                    angles[i] - 90,
+                )
+                shim_point3 = end_point_of_line(shim_point2, self.margin, angles[i])
+                shim_point4 = end_point_of_line(shim_point3, width, angles[i] + 90)
+                shim_point5 = end_point_of_line(
+                    shim_point4, self.margin, angles[i] + 180
+                )
+                shim_point6 = end_point_of_line(
+                    shim_point5,
+                    (self.beam_width - self.pannel_gap) / 2,
+                    angles[i] - 90,
+                )
+                end_shim = end_point_of_line(
+                    shim_point6, length_extremity_lines, angles[i] - 180
+                )
+                self.drawing.add(
+                    dxf.polyline(
+                        [
+                            start_point,
+                            shim_point1,
+                            shim_point2,
+                            shim_point3,
+                            shim_point4,
+                            shim_point5,
+                            shim_point6,
+                            end_shim,
+                        ]
+                    )
+                )
+        self.drawing.add(
+            dxf.line(
+                end_shim,
+                end_point_of_line(
+                    branch_position[0],
+                    (width - self.beam_width + self.pannel_gap) / 2,
+                    angles[0] - 90,
+                ),
+            )
+        )
+        self.drawing.save()
+
+    def __call__(self):
+        self.draw_shim()
+
+
+class ShimSperator:
+    def __init__(
+        self,
+        position: tuple[float],
+        angle: float,
+        ratio: float,
+        margin: float,
+        pannel_gap: 1.2,
+        beam_gap: 2.2,
+        beam_length=6.33,
+        beam_width=4.33,
+        drawing=dxf.drawing("yoshimora_shim.dxf"),
+    ):
+        self.position = position
+        self.angle = angle
+        self.ratio = ratio
+        self.margin = margin
+        self.pannel_gap = pannel_gap
+        self.beam_gap = beam_gap
+        self.beam_length = beam_length
+        self.beam_width = beam_width
+        self.drawing = drawing
+
+    def draw_shim_seperator(self):
+        width = self.beam_width * 1 / self.ratio
+        start_point = end_point_of_line(self.position, width / 2, self.angle - 90)
+        point1 = end_point_of_line(start_point, width, self.angle + 90)
+        point2 = end_point_of_line(point1, self.margin, self.angle)
+        point3 = end_point_of_line(
+            point2, (self.beam_width - self.pannel_gap) / 2, self.angle - 90
+        )
+        point4 = end_point_of_line(point3, self.beam_gap - self.margin, self.angle)
+        point5 = end_point_of_line(
+            point4, (self.beam_width - self.pannel_gap) / 2, self.angle + 90
+        )
+        point6 = end_point_of_line(point5, self.margin, self.angle)
+        point7 = end_point_of_line(point6, width, self.angle - 90)
+        point8 = end_point_of_line(point7, self.margin, self.angle + 180)
+        point9 = end_point_of_line(
+            point8, (self.beam_width - self.pannel_gap) / 2, self.angle + 90
+        )
+        point10 = end_point_of_line(
+            point9, self.beam_gap - self.margin, self.angle + 180
+        )
+        point11 = end_point_of_line(
+            point10, (self.beam_width - self.pannel_gap) / 2, self.angle - 90
+        )
+        point12 = end_point_of_line(point11, self.margin, self.angle + 180)
+        self.drawing.add(
+            dxf.polyline(
+                [
+                    point1,
+                    point2,
+                    point3,
+                    point4,
+                    point5,
+                    point6,
+                    point7,
+                    point8,
+                    point9,
+                    point10,
+                    point11,
+                    point12,
+                    point1,
+                ]
+            )
+        )
+        self.drawing.save()
+
+    def __call__(self):
+        self.draw_shim_seperator()
+
+
 class BuildingBlockYoshimora:
     def __init__(
         self,
@@ -355,28 +559,66 @@ class YoshimoraTesselation:
         self.draw_tesselation()
 
 
-if __name__ == "__main__":
-    tesselation = YoshimoraTesselation(
-        center=(0, 0),
-        size=(5, 5),
-        radius=2,
-        length=25,
-        angle=60,
-        count_beam=2,
-        pannel_gap=1.2,
-        drawing=dxf.drawing("test/yoshimura_tesselation.dxf"),
-    )
-    tesselation()
+class BuildingBlockShim:
+    def __init__(
+        self,
+        start_point: tuple[float],
+        length: float,
+        angle: float,
+        activated_branch=[True for _ in range(3)],
+        beam_gap=2.33,
+        beam_length=6.33,
+        beam_width=4.83,
+        drawing=dxf.drawing("yoshimura_shim.dxf"),
+    ):
+        pass
 
-    tesselation_tape = YoshimoraTesselation(
-        center=(200, 0),
-        size=(5, 5),
-        radius=2,
-        length=25,
-        angle=60,
-        count_beam=2,
-        pannel_gap=1.2,
-        drawing=tesselation.drawing,
-        tape=True,
+
+if __name__ == "__main__":
+    # tesselation = YoshimoraTesselation(
+    #     center=(0, 0),
+    #     size=(5, 5),
+    #     radius=2,
+    #     length=25,
+    #     angle=60,
+    #     count_beam=2,
+    #     pannel_gap=1.2,
+    #     drawing=dxf.drawing("test/yoshimura_tesselation.dxf"),
+    # )
+    # tesselation()
+
+    # tesselation_tape = YoshimoraTesselation(
+    #     center=(200, 0),
+    #     size=(5, 5),
+    #     radius=2,
+    #     length=25,
+    #     angle=60,
+    #     count_beam=2,
+    #     pannel_gap=1.2,
+    #     drawing=tesselation.drawing,
+    #     tape=True,
+    # )
+    # tesselation_tape()
+    activated_branch = [True for _ in range(6)]
+    activated_branch[0] = True
+    center_shim = CenterShim(
+        (0, 0),
+        2,
+        25,
+        44,
+        0.88,
+        0.67,
+        2,
+        1.2,
+        2.3,
+        beam_width=4.83,
+        beam_length=6.33,
+        activated_branch=activated_branch,
+        drawing=dxf.drawing("test/shim.dxf"),
     )
-    tesselation_tape()
+    center_shim()
+
+    sep_shim = ShimSperator(
+        (20, 0), 0, 0.88, 0.67, 1.2, 2.33, beam_width=4.83, drawing=center_shim.drawing
+    )
+    sep_shim()
